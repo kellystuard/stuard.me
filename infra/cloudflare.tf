abfcb9872ef1aa9e80b1948eb1a59502902a5cdd
kellystuard <kellystuard@gmail.com>
2023-12-16 14:40:38 -0600

    ... with the right zone

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -9,7 +9,7 @@ resource "cloudflare_zone" "stuard_me" {
 // DNS / Records
 
 resource "cloudflare_record" "cname_root" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = cloudflare_zone.stuard_me.id
   name    = "home"
   value   = "z1755512.eero.online"
   type    = "CNAME"
@@ -17,7 +17,7 @@ resource "cloudflare_record" "cname_root" {
 }
 
 resource "cloudflare_record" "cname_minecraft" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = cloudflare_zone.stuard_me.id
   name    = "minecraft"
   value   = "z1755512.eero.online"
   type    = "CNAME"
