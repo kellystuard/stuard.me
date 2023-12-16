faf278975b38d52ec4aa29b1ebcf0511be0998b4
kellystuard <kellystuard@gmail.com>
2023-12-16 12:37:56 -0600

    Don't hard-code things

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -9,7 +9,7 @@ resource "cloudflare_zone" "stuard_me" {
 // Rules / Settings
 
 resource "cloudflare_url_normalization_settings" "example" {
-  zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
+  zone_id = cloudflare_zone.stuard_me.id
   type    = "cloudflare"
   scope   = "incoming"
 }
